5a4f5f687a748ddd76b3089d6de9f17da240127b
Danielle Kom <daniellekom@Danielles-MacBook-Air.local>
2022-01-21 13:46:08 -0500

    function creation

---
+++ functions.js
@@ -0,0 +1,42 @@
+// // Use the keyword function followed by the name of the function.
+// // After the function name, open and close parentheses.
+// // After parenthesis, open and close curly braces.
+// // Within curly braces, write your lines of code.
+
+// function myDogsInfo(firstName,age,gender,favoriteToy){
+// console.log(`my Dogs name is ${firstName}`)
+// console.log(`my dog ${firstName} is ${age} years old`)
+// console.log(`${firstName} is a ${gender} and loves their toy ${favoriteToy}`)
+// }
+// console.log("------------------------------------------------------------")
+
+// myDogsInfo('skip',3,'girl','elmo')
+
+// function aboutMe(name,nickName,ageCount,genderSpecific,nationality){
+//     console.log(`Hello everyone, my name is ${name} but you can call me ${nickName} `)
+//     console.log(`I am ${ageCount} years old.`)
+//     console.log(`I identify as ${genderSpecific} and my nationality is ${nationality}`)
+// }
+// aboutMe('Danielle','Dani',30,'female','Israeli')
+
+// console.log("------------------------------------------------------------")
+
+// function sumof(num1,num2){
+//     let sumOfAns = num1+num2
+//     console.log(`The number ${num1} and ${num2} equals to ${sumOfAns}` )
+    
+// }
+// sumof(1,5)
+
+// console.log('--------------------------------------')
+
+//const usd=95 write a function to convert USD to another currency
+
+let usd = 95
+
+function currency(usd){
+    return usd * .71
+    
+}
+console.log(` ${usd} dollars is equivalent to ${currency(usd)} euros`)
+
